0ba640e414470d92dc2566d7844651c14c3790fb
numberwolf <porschegt23@foxmail.com>
2021-03-04 22:07:01 +0800

    FixMultiPlayerInNewCore

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,8 @@ export default class h265webjs {
 	static createPlayer(videoURL, config) {
 		return window.new265webjs(videoURL, config);
 	}
+
+	static clear() {
+        global.STATICE_MEM_playerCount = -1;
+    }
 }
