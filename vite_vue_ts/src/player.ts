866b6195a80459a9a43a357e1bfdc1fa7bdec25d
kanno <812137533@qq.com>
2022-04-26 18:38:12 +0800

    feat: 加入vite demo

---
+++ vite_vue_ts/src/player.ts
@@ -0,0 +1,56 @@
+const durationFormmat = (val) => {
+  val = val.toString()
+  if (val.length < 2) return '0' + val
+  return val
+}
+
+const setDurationText = (duration) => {
+  if (duration < 0) return 'Player'
+  const randDuration = Math.round(duration)
+  return (
+    durationFormmat(Math.floor(randDuration / 3600)) +
+    ':' +
+    durationFormmat(Math.floor((randDuration % 3600) / 60)) +
+    ':' +
+    durationFormmat(Math.floor(randDuration % 60))
+  )
+}
+
+export const createPlayerServer = (videoUrl, config) => {
+  // @ts-ignore
+  return window.new265webjs(videoUrl, config)
+}
+
+/**
+ * get playerInstance.
+ */
+export type PlayerInstance = ReturnType<typeof createPlayerServer>
+
+export const executePlayerServer = (player, timelineEl) => {
+  // todo....
+  let mediaInfo = null
+  player.onLoadFinish = () => {
+    mediaInfo = player.mediaInfo()
+    if (mediaInfo.videoType === 'vod') {
+      timelineEl.textContent =
+        setDurationText(0) +
+        '/' +
+        setDurationText(mediaInfo.meta.durationMs / 1000)
+    }
+  }
+  player.onPlayTime = (pts) => {
+    if (mediaInfo.videoType === 'vod') {
+      timelineEl.textContent =
+        setDurationText(pts) +
+        '/' +
+        setDurationText(mediaInfo.meta.durationMs / 1000)
+    }
+  }
+  player.do()
+}
+
+export const destoryPlayerServer = (playerInstance: PlayerInstance) => {
+  // release playerInstance & forece Gc
+  playerInstance.release()
+  playerInstance = null
+}
